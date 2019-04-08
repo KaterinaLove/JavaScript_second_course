4830712101289c0485c9f0713b15fec49f249f81
KaterinaLove <KaterinaLove@users.noreply.github.com>
2019-04-08 10:11:07 +0300

    КОНСТАНТА

--- Lesson1/script.js
+++ Lesson1/script.js
@@ -1,4 +1,4 @@
-const goods = [
+const GOODS = [
   {
     title: 'Shirt',
     price: 150
@@ -18,10 +18,10 @@ const goods = [
 ];
 
 but.onclick = function () {
-  for (var i = 0; i < goods.length; i++) {
+  for (var i = 0; i < GOODS.length; i++) {
     var div = document.createElement('div');
     div.className = "goods-list";
-    div.innerHTML = '<h3 class="title">' + goods[i].title + '</h3><p class="price">' + goods[i].price + '</p>';
+    div.innerHTML = '<h3 class="title">' + GOODS[i].title + '</h3><p class="price">' + GOODS[i].price + '</p>';
     main.appendChild(div);
   }
 };
@@ -33,10 +33,10 @@ const renderGoodsItem = (title, price) => {
 const renderGoodsList = () => {
   //map функция перебора исоздания нового массива 
   //goodsList - название нового массива
-  //goods - массив который мы перебираем
+  //GOODS - массив который мы перебираем
   //item - переменная стрелочной функции принимает значения от 0 до goods.length
   //renderGoodsItem - вызываемая функция в которую передается goods[item].title и записывается в goodsList
-  let goodsList = goods.map(item => renderGoodsItem(item.title, item.price));
+  let goodsList = GOODS.map(item => renderGoodsItem(item.title, item.price));
   //querySelector - вернется первый элемент в документе с классом goods-list
   //Выводятся запятые потому, что мы вывадим массив, а разделителем в массиве ясляется ','
   document.querySelector('.goods-list').innerHTML = goodsList;
@@ -46,9 +46,9 @@ renderGoodsList();
 
 //В этом примере убрала функцию генирирует часть будующего массива
 //перенесла ее в стрелочную функцию item и уменьнила код тем самым на 3 строки
-//Дабавила значения по умолчанию для аргументов функции (list = goods)
+//Дабавила значения по умолчанию для аргументов функции (list = GOODS)
 //join('') - выводит массив с указанным разделителем
-const renderGoodsList = (list = goods) => {
+const renderGoodsList = (list = GOODS) => {
   let goodsList = list.map(item => `<div class="goods-item"><h3>${item.title}</h3><p>${item.price}</p></div>`);
   document.querySelector('.goods-list').innerHTML = goodsList.join('');
 }
