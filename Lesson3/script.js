3a9c9094bdd93a552b62ca97086eb4378e8ec894
KaterinaLove <KaterinaLove@users.noreply.github.com>
2019-04-12 14:25:52 +0300

    Сделанно пол задания
    Я не поняла как работает промис, и у меня не получилось удолять и вставлять донные т.к. не понимаю как можно уточнить какой из товаров нужно удалить/добавить и как передать эти данные пакету JSON. Дополнительно прикрепила свою старую работу(может получется ее переделать)

--- Lesson3/script.js
+++ Lesson3/script.js
@@ -1,12 +1,13 @@
-//ресурс с продуктами в корзине
+//ресурс с JSON пакетами
 const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
-//
+//проверка соединения
 function makeGETRequest(url, callback) {
   var xhr;
+  console.log('Получение данных...');
 
   if (window.XMLHttpRequest) {
     xhr = new XMLHttpRequest();
-  } else if (window.ActiveXObject) { 
+  } else if (window.ActiveXObject) {
     xhr = new ActiveXObject("Microsoft.XMLHTTP");
   }
 
@@ -26,6 +27,7 @@ class ItemBasket {
     this.title = title; //имя элемента берется из бд(но у нас массив)
     this.price = price; //цена элемента берется из бд
   }
+  //вывод элемента(продукта)
   render() {
     return `
 <div class="goods-item">
@@ -33,25 +35,29 @@ class ItemBasket {
   <a href="#" class="goods-title">${this.title}</a>
   <p class="goods-price">${this.price} ₽</p>
   <div class="goods-button">
-    <button type="submit">
-      <svg style="width: 24px; height: 24px;" viewBox="0 0 22 19" xmlns="http://www.w3.org/2000/svg"><path d="M2.72 5.986c0-2.684 1.755-3.664 3.2-3.664 2.528 0 4.8 3.612 4.8 3.612s2.27-3.612 4.798-3.612c1.496 0 3.2.98 3.2 3.664 0 3.25-3.458 7.482-8.05 10.01-4.49-2.58-7.947-6.81-7.947-10.01zM15.57 0c-2.736 0-3.768 1.29-4.8 2.632C9.738 1.29 8.706 0 5.972 0 3.185 0 .45 2.012.45 5.986c0 4.644 4.8 9.958 10.32 12.59 5.522-2.632 10.32-7.946 10.32-12.59C21.09 2.012 18.356 0 15.57 0z" fill="currentColor" fill-rule="evenodd"></path></svg>
-    </button>
-    <button type="submit">
+    <button type="submit" id="fromBasket" onclick="list.from();">
      <svg style="width: 24px; height: 24px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 18"><g fill="none" fill-rule="evenodd"><path d="M-5-3h24v24H-5z"></path><path fill="currentColor" d="M10 4.001V16h1V4.001h1.999v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2.001-2v-12H3V16h1V4.001h2.5V16h1V4.001H10zM4.499 0L3.5 1.001H.999A1 1 0 0 0 0 2v1.001h14V2a1 1 0 0 0-1.001-.999H10.5L9.499 0h-5z"></path></g></svg>
     </button>
-    <button type="submit" class="goods-number">-</button>
-    <input autocomplete="off" type="number" maxlength="3" min="1" max="15" class="centered-text" setwidthbymaxlength="true">
-    <button type="submit" class="goods-number">+</button>
   </div>
 </div>`;
-  } //вывод элемента(продукта)
+  }
+  //вывод суммы цен элемента(продукта)
   costs(nam) {
     return `<div class="price"><p class="price-title">Итого: ${nam} ₽</p><button type="submit" class="price-button">Перейти к оформлению</button></div>`;
-  } //вывод суммы цен элемента(продукта)
+  }
+  //Вывод каталога продуктов
+  renderProducts() {
+    return `<a href="#" class="goods-title">${this.title}</a>
+<button type="submit" class="goods-number" id="inBasket" onclick="list.in();">
+В корзину
+</button>
+  <p class="goods-price">${this.price} ₽</p>`
+  }
 }
-//корзина
-class Basket {
-  constructor(cb) {
+
+//продукты
+class Products {
+  constructor() {
     this.products = []; //масив продуктов
   }
   fetchProducts(cb) {
@@ -62,31 +68,83 @@ class Basket {
   }
   render() {
     let listHtml = ''; //выводящая строчка
+    //перебор массива продуктов с созданием нового массива
+    this.products.forEach(item => {
+      const productItem = new ItemBasket(item.product_name, item.price); //создается новый массив
+      listHtml += productItem.renderProducts();
+    });
+    document.querySelector('.products').innerHTML = listHtml;
+  }
+}
+
+//корзина
+class Basket {
+  constructor() {
+    this.basket = []; //масив продуктов в карзине
+  }
+  fetchProducts() {
+    makeGETRequest(`${API_URL}/getBasket.json`, (products) => {
+      this.basket = JSON.parse(products);
+    })
+  }
+  render() {
+    let listHtml = ''; //выводящая строчка
     let money; //общая сумма товара
     let cost = ''; //выводит строчку с суммой
     let total = []; //масив только для цены
     //перебор массива продуктов с созданием нового массива
-    this.products.forEach(item => {
-      const productItem = new ItemBasket(item.product_name, item.price); //создается новый массив
-      listHtml += productItem.render(); //в новый массив записывается строчка из ItemBasket с подставленными значениями
-      total.push(item.price); //записываются цены в масив total
-      money = total.reduce((sum, current) => {
-        return sum + current;
-      }, 0); //суммирует данные из масива
-      cost = productItem.costs(money); //выводит строчку с суммой
-    });
-    document.querySelector('.goods-list').innerHTML = listHtml + cost; //вывадим все а див
+    if (this.basket.length != 0) {
+      this.basket.contents.forEach(item => {
+        const productItem = new ItemBasket(item.product_name, item.price); //создается новый массив
+        listHtml += productItem.render(); //в новый массив записывается строчка из ItemBasket с подставленными значениями
+        total.push(item.price); //записываются цены в масив total
+        money = total.reduce((sum, current) => {
+          return sum + current;
+        }, 0); //суммирует данные из масива
+        cost = productItem.costs(money); //выводит строчку с суммой
+      });
+      document.querySelector('.goods-list').innerHTML = listHtml + cost; //вывадим все а див
+    } else {
+      document.querySelector('.goods-list').innerHTML = 'Корзина пуста';
+    }
   }
+  // для удаления из корзины(ошибка: это не функция)
+  from() {
+      makeGETRequest(`${API_URL}/deleteFromBasket.json`, (products) => {
+        console.log(JSON)
+        //this.basket.splice(0, JSON.parse)
+      })
+    }
+    // для добовления в корзину(ошибка: это не функция)
+    in () {
+      makeGETRequest(`${API_URL}/addToBasket.json`, (products) => {
+        console.log(JSON)
+        //this.basket.push(JSON.parse)
+      })
+    }
 }
-//создаем новую карзину
+//создание каталога продуктов
+const prod = new Products();
+prod.fetchProducts(() => {
+  prod.render();
+});
+//создаем новую корзину
 const list = new Basket();
-  
-//открываем корзину поверх оасовного сайта
+list.fetchProducts();
+//открываем корзину поверх основного сайта
 buttonBasket.onclick = function () {
-  list.fetchProducts( () => {list.render();});
+  list.render();
   buttonBasket.className = "open-basket";
   buttonCloseBasket.className = "open-span";
 };
+//программа не знает о их существавании
+//fromBasket.onclick = function () {
+//  list.from();
+//}
+//inBasket.onclick = function () {
+//  list.in();
+//}
+
 //закрываем корзину
 buttonCloseBasket.onclick = function () {
   document.querySelector('.goods-list').innerHTML = "";
